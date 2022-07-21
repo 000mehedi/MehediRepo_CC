6100c77bd62ce2092a3f3a628bd7ee44ac9efa6a
mdabdullah.patwary <mdabdullah.patwary@IC-CS233-23.uc.ucalgary.ca>
2022-07-21 17:42:58 -0600

    Added title to main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
-
+			primaryStage.setTitle("Mehedi's Project");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
